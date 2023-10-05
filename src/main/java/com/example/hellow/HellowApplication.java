94a846e2e81a643c5a395312ab4b505dbdfe9d7d
zaineb.bachouch <zaineb.bachouch@esprit.tn>
2023-10-06 00:21:34 +0100

    Initial commit

---
+++ src/main/java/com/example/hellow/HellowApplication.java
@@ -0,0 +1,16 @@
+package com.example.hellow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HellowApplication.class, args);
+
+
+        System.out.println("zainebbbbbbbbbbbbbbbbbbb");
+    }
+
+}
---
+++ src/test/java/com/example/hellow/HellowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hellow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
