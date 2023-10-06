93653ffbdd2f706876400875f87b0147a19c7f6b
zaineb.bachouch <zaineb.bachouch@esprit.tn>
2023-10-06 01:28:48 +0100

    8

--- src/main/java/com/example/hellow/HellowApplication.java
+++ src/main/java/com/example/hellow/HellowApplication.java
@@ -10,7 +10,7 @@ public class HellowApplication {
         SpringApplication.run(HellowApplication.class, args);
 
 
-        System.out.println("zainebhhhhbcccbbbbbbbbbbbbbbbb");
+        System.out.println("zainebhhhhbcccbbbbbbbbbbbbbbb,b");
 
     }
 
